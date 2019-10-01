88111104c329fff5281732c5e2041aeb756adf79
AS <spb.as@pirlventures.com>
2019-10-01 13:09:43 +0300

    перенес выборку эелементов внутрь showWheel

--- scripts/wheel.js
+++ scripts/wheel.js
@@ -1,92 +1,100 @@
-
-const wheel = document.querySelector('.wheel');
-const smallReel = document.querySelector('.reel__inner--circle-small');
-const mediumReel = document.querySelector('.reel__inner--circle-medium');
-const contentReel = document.querySelector('.reel__inner--circle-content');
-
-const nameInput = document.querySelector('.wheel__input[name=name]');
-const phoneInput = document.querySelector('.wheel__input[name=tel]');
-const checkboxInput = document.querySelector('.wheel__checkbox');
-const continueBtn = document.querySelector('.wheel__button');
-const closeBtn = document.querySelector('.wheel__close');
-
 window.showWheel = (sectors, _cb) => {
-  contentReel.innerHTML = getSectorsHtml(sectors);
+  const wheel = document.querySelector('.wheel');
+  const smallReel = document.querySelector('.reel__inner--circle-small');
+  const mediumReel = document.querySelector('.reel__inner--circle-medium');
+  const contentReel = document.querySelector('.reel__inner--circle-content');
 
-  resizeWheel();
-  window.addEventListener('resize', resizeHandler);
+  const nameInput = document.querySelector('.wheel__input[name=name]');
+  const phoneInput = document.querySelector('.wheel__input[name=tel]');
+  const checkboxInput = document.querySelector('.wheel__checkbox');
+  const continueBtn = document.querySelector('.wheel__button');
+  const closeBtn = document.querySelector('.wheel__close');
 
-  nameInput.addEventListener('input', inputHandler);
-  phoneInput.addEventListener('input', inputHandler);
-  checkboxInput.addEventListener('input', inputHandler);
+  const init = () => {
+    wheel.style.opacity = 1;
 
-  continueBtn.addEventListener('click', () => spin(sectors));
+    contentReel.innerHTML = getSectorsHtml(sectors);
 
-  closeBtn.addEventListener('click', hideWheel);
+    resize();
+    window.addEventListener('resize', resizeHandler);
 
-  return hideWheel;
-};
+    nameInput.addEventListener('input', inputHandler);
+    phoneInput.addEventListener('input', inputHandler);
+    checkboxInput.addEventListener('input', inputHandler);
 
-const hideWheel = () => {
-  wheel.style.opacity = 0;
+    continueBtn.addEventListener('click', spin);
 
-  window.removeEventListener('resize', resizeHandler);
+    closeBtn.addEventListener('click', hide);
+  };
 
-  nameInput.removeEventListener('input', inputHandler);
-  phoneInput.removeEventListener('input', inputHandler);
-  checkboxInput.removeEventListener('input', inputHandler);
+  const hide = () => {
+    wheel.style.opacity = 0;
+  
+    window.removeEventListener('resize', resizeHandler);
+  
+    nameInput.removeEventListener('input', inputHandler);
+    phoneInput.removeEventListener('input', inputHandler);
+    checkboxInput.removeEventListener('input', inputHandler);
+  
+    continueBtn.removeEventListener('click', spin);
+    closeBtn.removeEventListener('click', hide);
+  
+    contentReel.removeEventListener('transitionend', spinStopHandler);
+  };
 
-  continueBtn.removeEventListener('click', spin);
-  closeBtn.removeEventListener('click', hideWheel);
+  const spin = () => {
+    continueBtn.removeEventListener('click', spin);
+    continueBtn.disabled = true;
+  
+    const availableSectors = sectors.filter(({ allow }) => allow);
+    const resultSector = availableSectors[random(availableSectors.length)];
+    const resultIndex = sectors.indexOf(resultSector);
+    const angle = (5 * 360) - (360 / sectors.length) * resultIndex;
+    const randomOffset = 0.8 * (Math.random() - 0.5) * (360 / sectors.length);
+  
+    contentReel.style.transform = `rotate(${angle + randomOffset}deg)`;
+    smallReel.style.transform = `rotate(${(4 + Math.random()) * 360}deg)`;
+    mediumReel.style.transform = `rotate(-${(4 + Math.random()) * 360}deg)`;
+  
+    contentReel.addEventListener('transitionend', spinStopHandler);
+  };
 
-  contentReel.removeEventListener('transitionend', spinStopHandler);
-};
+  const resize = () => {
+    const vertical = window.innerWidth < window.innerHeight;
+    const defaultWidth = vertical ? 800 : 800;
+    const defaultHeigth = vertical ? 800 : 800;
+  
+    const scaleX = window.innerWidth / defaultWidth;
+    const scaleY = window.innerHeight / defaultHeigth;
+    const scale = Math.min(1, scaleX, scaleY);
+  
+    if (vertical) {
+      wheel.classList.add('vertical');
+    } else {
+      wheel.classList.remove('vertical');
+    }
+    wheel.style.fontSize = `${scale}px`;
+  };
 
-const inputHandler = () => {
-  continueBtn.disabled = nameInput.value === '' || phoneInput.value === '' || !checkboxInput.checked;
-};
+  const resizeHandler = debounce(resize, 150);
 
-const spinStopHandler = () => {
-  continueBtn.disabled = false;
-};
+  const inputHandler = () => {
+    continueBtn.disabled = nameInput.value === '' || phoneInput.value === '' || !checkboxInput.checked;
+  };
 
-const spin = (sectors) => {
-  continueBtn.disabled = true;
+  const spinStopHandler = () => {
+    continueBtn.disabled = false;
+  };
 
-  const availableSectors = sectors.filter(({ allow }) => allow);
-  const resultSector = availableSectors[random(availableSectors.length)];
-  const resultIndex = sectors.indexOf(resultSector);
-  const angle = (5 * 360) - (360 / sectors.length) * resultIndex;
-  const randomOffset = 0.8 * (Math.random() - 0.5) * (360 / sectors.length);
+  init();
 
-  contentReel.style.transform = `rotate(${angle + randomOffset}deg)`;
-  smallReel.style.transform = `rotate(${(4 + Math.random()) * 360}deg)`;
-  mediumReel.style.transform = `rotate(-${(4 + Math.random()) * 360}deg)`;
-
-  contentReel.addEventListener('transitionend', spinStopHandler);
-};
-
-const resizeWheel = () => {
-  const vertical = window.innerWidth < window.innerHeight;
-  const defaultWidth = vertical ? 800 : 800;
-  const defaultHeigth = vertical ? 800 : 800;
-
-  const scaleX = window.innerWidth / defaultWidth;
-  const scaleY = window.innerHeight / defaultHeigth;
-  const scale = Math.min(1, scaleX, scaleY);
-
-  if (vertical) {
-    wheel.classList.add('vertical');
-  } else {
-    wheel.classList.remove('vertical');
-  }
-  wheel.style.fontSize = `${scale}px`;
+  return hide;
 };
 
 const getSectorsHtml = sectors => sectors
   .map(({ text }, i) => {
     const style = `transform: translateY(-50%) rotate(${(360/12) * i}deg)`;
-    const bigTextClass = /\%$/.test(text) ? 'reel__sector-text--big' : '';
+    const bigTextClass = /^[0-9]+\%$/.test(text) ? 'reel__sector-text--big' : '';
     return (
       `<div class="reel__sector" style="${style}">
         <span class="reel__sector-text ${bigTextClass}">${text}</span>
@@ -107,5 +115,3 @@ const debounce = (func, delay) => {
 };
 
 const random = (max) => Math.floor(Math.random() * max);
-
-const resizeHandler = debounce(resizeWheel, 150);
